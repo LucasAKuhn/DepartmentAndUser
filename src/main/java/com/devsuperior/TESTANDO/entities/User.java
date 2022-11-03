1c3db4227c7781794893a3839def84e41a76b5e0
LucasAKuhn <lucasa.kuhn@hotmail.com>
2022-11-02 22:10:23 -0300

    finish project

---
+++ src/main/java/com/devsuperior/TESTANDO/entities/User.java
@@ -0,0 +1,54 @@
+package com.devsuperior.TESTANDO.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    //User tem apenas um departamento
+    @ManyToOne
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    public User(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Department getDepartament() {
+        return department;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDepartament(Department department) {
+        this.department = department;
+    }
+}
